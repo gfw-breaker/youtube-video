60bc390924f42e3050b57f8aa26077c103eee006
gfw-breaker <lixiaoming0513@gmail.com>
2018-11-05 23:13:32 +0800

    -

--- dl.sh
+++ dl.sh
@@ -98,6 +98,8 @@ cat > $video_dir/$name.html << EOF
 <a href="http://$ip:10000/videos/709"><b>709维权律师大抓捕</b></a>&nbsp;&nbsp;
 <a href="http://$ip:10000/videos/res/TradeWar"><b>中美贸易战专题</b></a>&nbsp;&nbsp;
 <br/><br/>
+<a href="http://$ip:10000/videos/blog/report.html"><b>基本真相 - 迫害前大陆媒体对法轮功的报导</b></a>
+<br/><br/>
 <a href="http://$ip:10080/gb/8/11/24/n2339512.htm"><b>文昭：生活在希望中，做快乐的中国人</b></a>
 <br/><br/>
 <a href="http://$ip:10000/videos/blog/tuid.html"><b>三退大潮席卷全球 三亿人觉醒见证中共末日</b></a>
