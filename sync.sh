606f5e8d209842539b51c7efb6b8daafbcb1e092
gfw-breaker <lixiaoming0513@gmail.com>
2019-03-01 03:01:48 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 # author: gfw-breaker
 
 baseUrl="https://www.youtube.com"
+cwd=$(pwd)
 
 while read line; do
 	name=$(echo $line | cut -d',' -f1)
@@ -9,8 +10,5 @@ while read line; do
 	folder=$(echo $line | cut -d',' -f3)
 	sticky=$(echo $line | cut -d',' -f4)
 	channelUrl=$baseUrl/$id
-
-	mkdir -p /root/$folder
-	cd /root/$folder
-	bash dl.sh -f $folder -u $channelUrl -s $sticky
+	bash $cwd/dl.sh -f $folder -u $channelUrl -s $sticky
 done < channels.csv
