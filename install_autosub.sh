9b7f35b8137acc915fddbbc2df3609a7e0d67715
翻墙教练 <34854165+gfw-breaker@users.noreply.github.com>
2019-10-24 02:29:15 -0400

    Update install_autosub.sh

--- install_autosub.sh
+++ install_autosub.sh
@@ -7,5 +7,5 @@ pip install setuptools autosub
 wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/bin/youtube-dl
 chmod a+rx /usr/bin/youtube-dl
 
-wget https://raw.githubusercontent.com/gfw-breaker/youtube-video/master/convert_cc.sh - O convert_cc.sh
+wget https://raw.githubusercontent.com/gfw-breaker/youtube-video/master/convert_cc.sh -O convert_cc.sh
 
