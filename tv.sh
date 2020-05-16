32bf9f195de94093d474a4af3329cd3a2c62098e
gfw-breaker <lixiaoming0513@gmail.com>
2020-05-16 19:15:32 +0800

    ok

--- tv.sh
+++ tv.sh
@@ -5,7 +5,34 @@
 ip=141.164.41.95
 
 page=/usr/share/nginx/html/index.html
+yt=/usr/share/nginx/html/youtube.html
 
 sed -i "s#http.*m3u8#http://$ip:8009/cn/live800/playlist.m3u8#" $page
 
+cd /root/youtube-video
+
+cat > $yt << EOF
+<html>
+<head>
+<meta charset="utf-8" /> 
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title> YouTube 节目列表 </title>
+<style>
+a {
+	margin: 60px;
+	line-height: 160%;
+}
+</style>
+</head>
+<body>
+<br/>
+<b>
+<a href='/'>新唐人电视直播</a><br/>
+EOF
+
+while read line ; do
+	title=$(echo $line | cut -d',' -f1)
+	folder=$(echo $line | cut -d',' -f3)
+	echo "<a href='/$folder/'>$title</a><br/>" >> $yt
+done < channels.csv
 
