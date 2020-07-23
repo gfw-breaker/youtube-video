e3112994f0cf6390dbc16de713eb360b911e2aa4
gfw-breaker <lixiaoming0513@gmail.com>
2020-07-23 16:11:38 +0800

    ok

--- sync.sh
+++ sync.sh
@@ -121,9 +121,9 @@ EOF
 	done < /root/youtube-video/hot.txt	
 
 	abc=$(curl -s https://raw.githubusercontent.com/begood0513/goodnews/master/indexes/ABC.csv | head -n 1)
-	linkUrl=$(echo $abc | cut -d',' -f1 | sed "s#https://www.ntdtv.com#http://ip:8808#" | sed "s#https://www.epochtimes.com#http://ip:10080#")
+	linkUrl=$(echo $abc | cut -d',' -f1 | sed "s#https://www.ntdtv.com#http://$ip:8808#" | sed "s#https://www.epochtimes.com#http://$ip:10080#")
 	linkTitle=$(echo $abc | cut -d',' -f2)
-	echo "<div><a href='$linkUrl'>🔥 $tlinkTitle</a></br></div>" >> $index_page
+	echo "<div><a href='$linkUrl'>🔥 $tinkTitle</a></br></div>" >> $index_page
 
 	while read video; do
 			id=$(echo $video | cut -d'|' -f1)
