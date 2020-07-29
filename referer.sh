aa66661e1d1b80ee2b85570e7810911cab34eb15
gfw-breaker <lixiaoming0513@gmail.com>
2020-07-29 08:12:41 +0800

    ok

--- referer.sh
+++ referer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-c1=$(grep fromvideos /var/log/nginx/access.log | grep "GET /gb/" | awk '{ print $1 }' | sort | uniq | wc -l)
+c1=$(grep fromvideos /var/log/nginx/access.log | grep -v " 200 0 " | grep "GET /gb/" | awk '{ print $1 }' | sort | uniq | wc -l)
 
-c2=$(egrep "singlemessage|groupmessage" /var/log/nginx/access.log  | awk '{ print $1 }' | sort | uniq  | wc -l)
+c2=$(egrep "singlemessage|groupmessage" /var/log/nginx/access.log  | grep -v " 200 0 " | awk '{ print $1 }' | sort | uniq  | wc -l)
 
-c3=$(egrep "nsukey" /var/log/nginx/access.log  | awk '{ print $1 }' | sort | uniq  | wc -l)
+c3=$(egrep "nsukey" /var/log/nginx/access.log | grep -v " 200 0 " | awk '{ print $1 }' | sort | uniq  | wc -l)
 
-c4=$(grep -v '" 403 ' /var/log/nginx/access.log  | awk '{ print $1 }' | sort | uniq  | wc -l)
+c4=$(grep -v '" 403 ' /var/log/nginx/access.log | grep -v " 200 0 " | awk '{ print $1 }' | sort | uniq  | wc -l)
 
 echo "From YouTube : $c1"
 echo "From WeChat  : $c2"
