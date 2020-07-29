59b675649a04b6e7aa097fa86973a2f7af866cfb
gfw-breaker <lixiaoming0513@gmail.com>
2020-07-29 10:49:25 +0800

    ok

--- share.sh
+++ share.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-egrep "singlemessage|groupmessage" /var/log/nginx/access.log  | awk '{ print $1","$11 }' | cut -d'?' -f1 | sed 's/"//g' | grep -v ',-' | sort | uniq | cut -d',' -f2
+egrep "singlemessage|groupmessage" /var/log/nginx/access.log  | awk '{ print $1","$11 }' | cut -d'?' -f1 | sed 's/"//g' | grep -v ',-' | sort | uniq | cut -d',' -f2 | sort | uniq -c | sort -n
 
