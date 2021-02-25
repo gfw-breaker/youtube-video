7c10400b0119168dc9ddb450f57e2c8ae6ab1c6b
gfw-breaker <lixiaoming0513@gmail.com>
2021-02-25 19:11:18 +0800

    ok

--- tv.sh
+++ tv.sh
@@ -32,8 +32,9 @@ a {
 <body>
 <br/>
 <b>
+<!--
 <a href='http://$ip:11000/show.aspx?name=ogHome'>网门免翻墙，一键浏览全球精粹资源 头条、影视、音乐、书刊、直播</a><br/>
-
+-->
 EOF
 
 #<a href='http://$ip:10000/videos/res2/djy-news/'>大紀元新聞網YouTube频道</a><br/>
