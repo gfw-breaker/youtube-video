82c19c6cae5172f86f936ad9053ae3979dad46c8
gfw-breaker <lixiaoming0513@gmail.com>
2021-05-09 10:35:07 +0800

    ok

---
+++ qr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+port=81
+videoHome=/usr/share/nginx/html
+
+yum install -y qrencode
+
+ip=$(/sbin/ifconfig eth0 | grep "inet addr" | sed -n 1p | cut -d':' -f2 | cut -d' ' -f1)
+if [ -z $ip ]; then
+	ip=$(/sbin/ifconfig eth0 | grep "broadcast" | awk '{print $2}')
+fi
+
+cd /root/youtube-video
+
+ts=$(date "+%m%d%H%m")
+
+while read line ; do
+	title=$(echo $line | cut -d',' -f1)
+	folder=$(echo $line | cut -d',' -f3)
+	url="http://$ip:port/$folder/?t=$ts"
+	echo $url
+done < channels.csv
+
+
